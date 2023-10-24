c1a8da4be30d806f2731d6f63cf41eded9be0c56
VORLEAK.KUN <VORLEAK.KUN@PNC.LAN>
2023-10-24 15:40:52 +0700

    #3 create function for story back introduction gamestart

--- game.py
+++ game.py
@@ -57,7 +57,18 @@ all_level_bg= PhotoImage(file="image/all-levels.png")
 
 
 #=========================== ALL LEVELS =======================
+def alllevels():
+    canvas.delete("all")
+    canvas.create_image(1,0, image = all_level_bg, anchor = "nw")
+    # ______________________________LEVEL1_____________________________________
+    canvas.create_image(200,250, image = level1_list, anchor = "nw", tags="level1-")
+    # ______________________________LEVEL2_______________________________________
+    canvas.create_image(565,250, image = level2_list, anchor = "nw", tags="level2-")
+    # ______________________________LEVEL3_______________________________________
+    canvas.create_image(950,250, image = level3_list, anchor = "nw", tags="level3-")
+    canvas.create_image(20,20, image =button_exist, anchor = "nw",tags="backhome")
 
+#======================== LEVEL1 =======================
 def level01(event):
     canvas.delete("all")
     global player
@@ -82,11 +93,11 @@ def level01(event):
     # ==================  PLAYER ===============
     
 # def level2(event):
-#     canvas.create_image(1, 0, image=summer_bg, anchor="nw")
+#     canvas.create_image(1, 0, image=level2, anchor="nw")
 #     canvas.create_image(25, 10, image=back_img, anchor="nw", tags="back_all_levels")
 
 # def level3(event):
-#     canvas.create_image(1, 0, image=level3_bg, anchor="nw")
+#     canvas.create_image(1, 0, image=level3, anchor="nw")
 #     canvas.create_image(25, 10, image=back_img, anchor="nw", tags="back_all_levels")
 
 # ---------------------------------------------------------------------------
@@ -96,25 +107,48 @@ def level01(event):
 def story(event):
     canvas.delete("all")
     canvas.create_image(1, 0, image=game_story, anchor = 'nw')
+    canvas.create_image(20,20, image =button_exist, anchor = "nw",tags="backhome")
 
-
+# ==============> BACK <==================
+def back(event):
+    canvas.delete("all")
+    home()
+# ==============> Indroduction <==================
+def introdution(event):
+    canvas.delete("all")
+    canvas.create_image(1, 0, image=game_introduction, anchor = 'nw')
+    canvas.create_image(20,20, image =button_exist, anchor = "nw",tags="backhome")
+# ---------------------------------------------------------------------------
 #=> CREATE GAME SHOW
 # ---------------------------------------------------------------------------
 def home():
-    canvas.create_image(680, 372, image=game_start)
-    canvas.create_image(700,670, image=button_play, tags="startgame")
-    canvas.create_image(500,665,image=all_levels, tags="level")
-    canvas.create_image(900,670,image=button_help, tags="help")
-    # winsound.PlaySound("sound/open.wav",winsound.SND_FILENAME | winsound.SND_ASYNC)
+    canvas.create_image(1, 0, image=game_start, anchor='nw')
+    canvas.create_image(500,500,image=story_list, tags="story-of-game")
+    canvas.create_image(700,500, image=button_play, tags="startgame")
+    canvas.create_image(900,500,image=button_help, tags="help")
+
+####### START GAME ######3
+def startGame(event):
+    canvas.delete('all')
+    showSlid1()
+def showSlid1():
+    canvas.create_image(1,0,image=slide1,anchor='nw')
+    canvas.after(1000,showSlid2)
+def showSlid2():
+    canvas.create_image(1,0,image=slide2, anchor='nw')
+    canvas.after(1000,showSlid3)
+def showSlid3():
+    canvas.create_image(1,0,image=slide3, anchor='nw')
+    canvas.create_text(700,420,text="Loading...",font=('sansarif',28,'bold'),fill='white')
+    canvas.after(2000,alllevels)
 
 #=> ALLOW WINDOWS KEYS AND TAGES BIND
 # ---------------------------------------------------------------------------
-# canvas.tag_bind("help","<Button-1>",introdution )
-# canvas.tag_bind("story","<Button-1>", story)
-# canvas.tag_bind("story","<Button-1>", level01)
-# canvas.tag_bind("backhome","<Button-1>", back)
-# canvas.tag_bind("startgame","<Button-1>", startGame )
-# canvas.tag_bind("level1-","<Button-1>", level01 )
+canvas.tag_bind("help","<Button-1>",introdution )
+canvas.tag_bind("story-of-game","<Button-1>", story)
+canvas.tag_bind("backhome","<Button-1>", back)
+canvas.tag_bind("startgame","<Button-1>", startGame )
+canvas.tag_bind("level1-","<Button-1>", level01 )
 # canvas.tag_bind("level2-","<Button-1>", level02 )
 # canvas.tag_bind("level3-","<Button-1>", level03 )
 
